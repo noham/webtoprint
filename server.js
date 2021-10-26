82a77e6d28bc926f3eb05d9405d59ee1101d2448
David Mahon <davidmahon@Davids-MacBook-Pro-2.local>
2021-10-26 15:35:57 +0100

    added express basic auth

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const basicAuth = require('express-basic-auth')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
@@ -14,8 +15,13 @@ db.on('error', (error)=> console.error(error))
 db.once('open', ()=> console.log('connected to db'))
 
 //middleware
+
 app.use(cors())
 app.use(express.json())
+app.use(basicAuth({
+  users: { 'username' : process.env.PASSWORD }
+}
+)) 
 
 // routes
 const measurementsRouter = require('./routes/measurements')
