d6e3ad012c1517a5340e3eddd3240845432d9331
David Mahon <davidmahon@Davids-MacBook-Pro-2.local>
2021-11-12 16:56:30 +0000

    updated password matching

--- routes/users.js
+++ routes/users.js
@@ -11,18 +11,18 @@ router.get('/', async (req, res)=>{
         // const user = await User.find()
         // res.json(user)
 
-        let username = req.body.username
-        let password = req.body.password
+        // let username = req.body.username
+        // let password = req.body.password
 
-        module.exports = {
-            loginUser: function(username, password, callback) {
-              UserModel.findOne({username: username}).exec(function(error, user) {
+        // module.exports = {
+        //     loginUser: function(username, password, callback) {
+              User.findOne({username: req.body.username}).exec(function(error, user) {
                 if (error) {
                   callback({error: true})
                 } else if (!user) {
                   callback({error: true})
                 } else {
-                  user.comparePassword(password, function(matchError, isMatch) {
+                  user.comparePassword(req.body.password, function(matchError, isMatch) {
                     if (matchError) {
                       callback({error: true})
                     } else if (!isMatch) {
@@ -33,8 +33,8 @@ router.get('/', async (req, res)=>{
                   })
                 }
               })
-            }
-          }
+        //     }
+        //   }
 
 
 
