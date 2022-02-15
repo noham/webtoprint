4f52e6e0964dda0e9ffdd9c3fb1b739e8b008540
David Mahon <davidmahon@Davids-MacBook-Pro-2.local>
2022-02-15 16:20:23 +0000

    Update user.js

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
+        required: false
     },
     dateCreated: {
         type: Date,
