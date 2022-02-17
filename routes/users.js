ceee873b7b6564e78de5797bd511752311ce5451
David Mahon <davidmahon@Davids-MacBook-Pro-2.local>
2022-02-17 16:40:33 +0000

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -32,7 +32,7 @@ router.get('/', async (req, res)=>{
                     } else if (!isMatch) {
                         res.json({user: false})
                     } else {
-                        res.json({associatedPub: res.associatedPub})
+                        res.json(res.user)
                     }
                     })
             }
