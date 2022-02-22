909ca096792490671bc254b34836d3a88198fc67
David Mahon <davidmahon@Davids-MacBook-Pro-2.local>
2022-02-22 11:56:25 +0000

    changed back the user model

--- routes/users.js
+++ routes/users.js
@@ -32,7 +32,7 @@ router.get('/', async (req, res)=>{
                     } else if (!isMatch) {
                         res.json({user: false})
                     } else {
-                        res.json({user: true})
+                        res.json(user)
                     }
                     })
             }
